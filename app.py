7a196d3173429b0b471887d1ba8d155c9b5de8e9
mayank-1234567 <220713980+mayank-1234567@users.noreply.github.com>
2026-07-16 19:41:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,32 @@ import streamlit as st
 from config.config import Config
 from core.database import DatabaseManager
 from services.profile_service import ProfileService
+
 from ui.profile_page import first_time_setup
+from ui.dashboard import show_dashboard
 
 st.set_page_config(
+
     page_title=Config.APP_NAME,
+
     page_icon="🚀",
+
     layout="wide"
+
 )
 
 db = DatabaseManager()
+
 db.initialize_database()
 
 profile = ProfileService()
 
-if not profile.profile_exists():
-
-    first_time_setup()
-
-else:
+if profile.profile_exists():
 
     user = profile.get_profile()
 
-    st.title(f"👋 Welcome {user[1]}")
+    show_dashboard(user)
 
-    st.success("Profile Loaded Successfully")
+else:
 
-    st.write("Dashboard coming in the next phase.")
+    first_time_setup()
